fdb33592cdc3259b20fa0e87aa8b2b77a9a1ba7a
이은총 <aass6863@naver.com>
2025-12-07 21:49:15 +0900

    프론트 연결

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -3,7 +3,7 @@
 
 import { io } from "socket.io-client";
 
-const URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:4000";
+const URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000";
 
 export const socket = io(URL, {
   autoConnect: false, // 수동 실행
